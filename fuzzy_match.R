e68e0c962a4bc04e7fb182ef5cbfd6cf66ed5cd6
Gaurav Gurjar <55551769+yourfriend-gaurav-gurjar@users.noreply.github.com>
2020-05-01 01:43:15 -0400

    Update fuzzy_match.R

--- fuzzy_match.R
+++ fuzzy_match.R
@@ -1,13 +1,21 @@
+# Loading the libraries
 library(dplyr)
 library(tidyverse)
 library(fuzzyjoin)
+library(openxlsx)
 
-path <- "/fuzzy_match_working_file.xlsx"
+# Loading the xlsx file 
+path <- "/fuzzy_match.xlsx"
+
+# Extracting the spreadsheets from xlsx file
 sheet1_data_tableA <- readxl::read_xlsx(path, sheet = 1)
 sheet2_data_tableB <- readxl::read_xlsx(path, sheet = 2)
 sheet3_data_tableC <- readxl::read_xlsx(path, sheet = 3)
 
+# Renaming column names of sheet1_data_tableA
+names(sheet1_data_tableA)[names(sheet1_data_tableA) == "player_name"] <- "player" 
 
+## Fuzzy match 
 final_result <- stringdist_join(sheet1_data_tableA, sheet2_data_tableB, by = "player", mode = "left", ignore_case = FALSE, method = "jw", p = .15, max_dist = 4, distance_col = "dist") %>%
   group_by(player.x) %>%
   top_n(1, -dist) %>%
@@ -25,9 +33,24 @@ final_result <- stringdist_join(sheet1_data_tableA, sheet2_data_tableB, by = "pl
   unite("player_name_data_table_B", fname.y, lname.y, sep = ". ") %>%
   select(-count.x, -count.y)
   
+# fuzzy_match_confidence
 final_result$dist = 1-final_result$dist
+
+
+# Renaming column names of final_result
 title_col <- colnames(sheet3_data_tableC)
 names(final_result)[names(final_result) == "player_id.x"] <- title_col[1]
 names(final_result)[names(final_result) == "player_id.y"] <- title_col[2]
 names(final_result)[names(final_result) == "dist"] <- title_col[5]
-final_result
+
+## Create a excel file
+fuzzy_match <- createWorkbook()
+addWorksheet(fuzzy_match, "data_table_A")
+addWorksheet(fuzzy_match, "data_table_B")
+addWorksheet(fuzzy_match, "data_table_C")
+writeData(fuzzy_match, 1, sheet1_data_tableA)
+writeData(fuzzy_match, 2, sheet2_data_tableB)
+writeData(fuzzy_match, 3, final_result)
+saveWorkbook(fuzzy_match, file = "fuzzy_match[Gaurav-UpWork].xlsx")
+saveWorkbook(fuzzy_match, file = "/home/gurjar/Downloads/fuzzy_match_working_file.xlsx", overwrite = TRUE)
+
